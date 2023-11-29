64bda026241f794c4489e295638c7a0c4b4d9d39
Sreejith.R <72004869+sreejithSHR@users.noreply.github.com>
2023-11-29 12:30:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,19 +19,16 @@ def apply_edge_detection(img, edge_method):
     if edge_method == 'Canny':
         return cv2.Canny(img, 100, 200)
     elif edge_method == 'LOG':
-        # Apply Laplacian of Gaussian (LoG)
         gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         log_result = cv2.Laplacian(cv2.GaussianBlur(gray_img, (5, 5), 0), cv2.CV_64F)
         normalized_log_result = (log_result - log_result.min()) / (log_result.max() - log_result.min())
         return normalized_log_result
     elif edge_method == 'DOG':
-        # Apply Difference of Gaussians (DoG)
         blurred_img1 = cv2.GaussianBlur(img, (3, 3), 0)
         blurred_img2 = cv2.GaussianBlur(img, (5, 5), 0)
         return blurred_img1 - blurred_img2
     else:
         return img
-
 
 def apply_filter(img, filter_type, size):
     if filter_type == 'Gaussian Blur':
@@ -54,17 +51,16 @@ def main():
         file_bytes = np.asarray(bytearray(image_file.read()), dtype=np.uint8)
         img = cv2.imdecode(file_bytes, 1)
 
-        # Sidebar
-        st.sidebar.title("MENU")
-        operation_type = st.sidebar.selectbox('Select Operation Type', ['Basic Operations', 'Edge Detection Techniques'])
+        st.subheader('Select Operation Type:')
+        operation_type = st.radio('', ['Basic Operations', 'Edge Detection Techniques'])
 
         if operation_type == 'Basic Operations':
-            st.sidebar.subheader('Basic Operations')
-            basic_operation = st.sidebar.radio('Select Operation', ['None', 'Invert', 'Grayscale', 'Resize', 'Rotate'])
+            st.subheader('Basic Operations')
+            basic_operation = st.radio('Select Operation', ['None', 'Invert', 'Grayscale', 'Resize', 'Rotate'])
             output = apply_basic_operations(img, basic_operation)
         elif operation_type == 'Edge Detection Techniques':
-            st.sidebar.subheader('Edge Detection Techniques')
-            edge_method = st.sidebar.radio('Select Edge Detection Method', ['None', 'Canny', 'LOG', 'DOG'])
+            st.subheader('Edge Detection Techniques')
+            edge_method = st.radio('Select Edge Detection Method', ['None', 'Canny', 'LOG', 'DOG'])
             output = apply_edge_detection(img, edge_method)
         else:
             output = img
