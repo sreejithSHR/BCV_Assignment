9d3a682da21be20f626366cc40e2cc0c3bb9158a
Sreejith.R <72004869+sreejithSHR@users.noreply.github.com>
2023-11-29 12:29:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,66 +1,81 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Mar  1 22:00:26 2023
-
-@author: Mithun
-"""
-from flask import Flask, request, jsonify
-import pandas as pd
-import pickle
-import numpy as np
 import cv2
-import mediapipe as mp
-from collections import Counter
+import numpy as np
+import streamlit as st
 
-app = Flask(__name__)
+def apply_basic_operations(img, operation):
+    if operation == 'Invert':
+        return 255 - img
+    elif operation == 'Grayscale':
+        return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    elif operation == 'Resize':
+        dim = (1920, 1080)
+        return cv2.resize(img, dim)
+    elif operation == 'Rotate':
+        return cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
+    else:
+        return img
 
-# Load the saved model
-with open('knn_model.pkl', 'rb') as file:
-    model = pickle.load(file)
+def apply_edge_detection(img, edge_method):
+    if edge_method == 'Canny':
+        return cv2.Canny(img, 100, 200)
+    elif edge_method == 'LOG':
+        # Apply Laplacian of Gaussian (LoG)
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        log_result = cv2.Laplacian(cv2.GaussianBlur(gray_img, (5, 5), 0), cv2.CV_64F)
+        normalized_log_result = (log_result - log_result.min()) / (log_result.max() - log_result.min())
+        return normalized_log_result
+    elif edge_method == 'DOG':
+        # Apply Difference of Gaussians (DoG)
+        blurred_img1 = cv2.GaussianBlur(img, (3, 3), 0)
+        blurred_img2 = cv2.GaussianBlur(img, (5, 5), 0)
+        return blurred_img1 - blurred_img2
+    else:
+        return img
 
-# Initialize the MediaPipe Pose model
-mp_pose = mp.solutions.pose
-pose = mp_pose.Pose(static_image_mode=True, min_detection_confidence=0.5)
 
-# Define an endpoint for the pose classification API
-@app.route('/classify-pose', methods=['POST'])
-def classify_pose():
-    # Get the uploaded video file
-    file = request.files['file']
-    
-    # Load the video file using OpenCV and extract the number of frames
-    cap = cv2.VideoCapture(file)
-    num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    
-    # Define an empty DataFrame to store the x, y, z, and visibility data
-    columns = ['Frame', 'Joint', 'X', 'Y', 'Z', 'Visibility']
-    df = pd.DataFrame(columns=columns)
+def apply_filter(img, filter_type, size):
+    if filter_type == 'Gaussian Blur':
+        return cv2.GaussianBlur(img, (size, size), 0)
+    elif filter_type == 'Median Blur':
+        return cv2.medianBlur(img, size)
+    elif filter_type == 'Bilateral':
+        return cv2.bilateralFilter(img, size, 75, 75)
+    else:
+        return img
 
-    # Iterate through each frame of the video and use MediaPipe to detect the pose landmarks
-    for frame in range(num_frames):
-        ret, image = cap.read()
-        if not ret:
-            break
+def main():
+    st.title('Image Filtering & Operations')
 
-        # Convert the image to RGB and process it with MediaPipe
-        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        results = pose.process(image_rgb)
+    # Upload image
+    image_file = st.file_uploader("Choose and upload an image", type=['jpg', 'jpeg', 'png'])
 
-        # Extract the landmark data and store it in the DataFrame
-        if results.pose_landmarks:
-            landmarks = [[frame, i, lm.x, lm.y, lm.z, lm.visibility] for i, lm in enumerate(results.pose_landmarks.landmark)]
-            df = df.append(pd.DataFrame(landmarks, columns=columns), ignore_index=True)
+    if image_file is not None:
+        # Load image
+        file_bytes = np.asarray(bytearray(image_file.read()), dtype=np.uint8)
+        img = cv2.imdecode(file_bytes, 1)
 
-    # Make predictions using the KNN model
-    test_set = df.iloc[:, 2:].values
-    ypred = model.predict(test_set)
+        # Sidebar
+        st.sidebar.title("MENU")
+        operation_type = st.sidebar.selectbox('Select Operation Type', ['Basic Operations', 'Edge Detection Techniques'])
 
-    # Get the counts of each predicted label
-    counts = Counter(ypred)
-    
-    # Return the counts as a JSON response
-    return jsonify(counts)
+        if operation_type == 'Basic Operations':
+            st.sidebar.subheader('Basic Operations')
+            basic_operation = st.sidebar.radio('Select Operation', ['None', 'Invert', 'Grayscale', 'Resize', 'Rotate'])
+            output = apply_basic_operations(img, basic_operation)
+        elif operation_type == 'Edge Detection Techniques':
+            st.sidebar.subheader('Edge Detection Techniques')
+            edge_method = st.sidebar.radio('Select Edge Detection Method', ['None', 'Canny', 'LOG', 'DOG'])
+            output = apply_edge_detection(img, edge_method)
+        else:
+            output = img
+
+        st.subheader('Filters')
+        filter_type = st.radio('Select Filter', ['None', 'Gaussian Blur', 'Median Blur', 'Bilateral'])
+        filter_size = st.slider('Filter Size', 3, 11, 5, 2)
+        filtered_img = apply_filter(img, filter_type, filter_size)
+
+        # Display images
+        st.image([img, filtered_img, output], width=300, caption=['Original Image', 'Filtered Image', 'Processed Image'])
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    main()
